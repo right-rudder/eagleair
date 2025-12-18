569989a0a9529f6aa8363ddefcc347c698b33299
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-12-18 11:44:18 -0300

    Update fleet images

---
+++ src/assets/fleet/fleetImages.js
@@ -0,0 +1,38 @@
+const fleetImages = [
+  {
+    src: "/src/assets/fleet/fleet-cesna-N14BL_Eagle-air_Indiana-USA.jpg",
+    alt: "Fleet Cessna N14BL, Eagle Air Indiana USA",
+  },
+  {
+    src: "/src/assets/fleet/fleet-cesna-N745N_eagle-air_indiana-USA_valparaiso-IN_Flight-school_pilot-training.jpg",
+    alt: "Fleet Cessna N745N, Eagle Air Indiana USA Valparaiso IN flight school pilot training",
+  },
+  {
+    src: "/src/assets/fleet/fleet-cesna-N745N_front-ground_eagle-air_indiana-USA_valparaiso-IN_Flight-school_pilot-training.jpg",
+    alt: "Fleet Cessna N745N front ground view, Eagle Air Indiana USA Valparaiso IN flight school pilot training",
+  },
+  {
+    src: "/src/assets/fleet/fleet-cesna-N745N_front-ground-2_eagle-air_indiana-USA_valparaiso-IN_Flight-school_pilot-training.jpg",
+    alt: "Fleet Cessna N745N front ground view 2, Eagle Air Indiana USA Valparaiso IN flight school pilot training",
+  },
+  {
+    src: "/src/assets/fleet/fleet-cesna-N745U_eagle-air_indiana-USA_valparaiso-IN_Flight-school_pilot-training.jpg",
+    alt: "Fleet Cessna N745U, Eagle Air Indiana USA Valparaiso IN flight school pilot training",
+  },
+  {
+    src: "/src/assets/fleet/fleet-cesna-N4347R_Eagle-air_Indiana-USA.jpg",
+    alt: "Fleet Cessna N4347R, Eagle Air Indiana USA",
+  },
+  {
+    src: "/src/assets/fleet/fleet-cesna-N6896H_Eagle-air_Indiana-USA.jpg",
+    alt: "Fleet Cessna N6896H, Eagle Air Indiana USA",
+  },
+  {
+    src: "/src/assets/fleet/fleet-cesna-N7186S_Eagle-air_Indiana-USA.jpg",
+    alt: "Fleet Cessna N7186S, Eagle Air Indiana USA",
+  },
+  {
+    src: "/src/assets/fleet/fleet-plane-concept_eagle-air_indiana-USA_valparaiso-IN_Flight-school_pilot-training.jpg",
+    alt: "Fleet plane concept, Eagle Air Indiana USA Valparaiso IN flight school pilot training",
+  },
+];
