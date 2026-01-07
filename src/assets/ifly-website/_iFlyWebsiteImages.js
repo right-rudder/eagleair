cf9569081bbf42dc75f990411820a75b3ad1c278
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2026-01-07 18:32:47 -0300

    Create _iFlyWebsiteImages.js

---
+++ src/assets/ifly-website/_iFlyWebsiteImages.js
@@ -0,0 +1,62 @@
+const images = [
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_banner_contacto.webp",
+    alt: "Partner Eagle Aircraft in Indiana USA - iFly Flight School Pilot Training Banner Contact",
+  },
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_ifly_bg_qs_002.webp",
+    alt: "iFly Background QS 002",
+  },
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_ifly_escuela_de_vuelo_pilotos_005.jpg",
+    alt: "iFly Flight School Pilots 005",
+  },
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_ifly_escuela_de_vuelo_pilotos_024.jpg",
+    alt: "iFly Flight School Pilots 024",
+  },
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_ifly_escuela_de_vuelo_pilotos_027.jpg",
+    alt: "iFly Flight School Pilots 027",
+  },
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_ifly_escuela_de_vuelo_pilotos_029.jpg",
+    alt: "iFly Flight School Pilots 029",
+  },
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_ifly_escuela_de_vuelo_pilotos_036.jpg",
+    alt: "iFly Flight School Pilots 036",
+  },
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_ifly_escuela_de_vuelo_pilotos_040.jpg",
+    alt: "iFly Flight School Pilots 040",
+  },
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_ifly_escuela_de_vuelo_pilotos_044.jpg",
+    alt: "iFly Flight School Pilots 044",
+  },
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_ifly_escuela_de_vuelo_pilotos_046.jpg",
+    alt: "iFly Flight School Pilots 046",
+  },
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_ifly_escuela_de_vuelo_pilotos_047.jpg",
+    alt: "iFly Flight School Pilots 047",
+  },
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_ifly_escuela_de_vuelo_pilotos_048.jpg",
+    alt: "iFly Flight School Pilots 048",
+  },
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_ifly_escuela_de_vuelo_pilotos_049.jpg",
+    alt: "iFly Flight School Pilots 049",
+  },
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_ifly_escuela_de_vuelo_pilotos_056.jpg",
+    alt: "iFly Flight School Pilots 056",
+  },
+  {
+    src: "src/assets/ifly-website/partner-eagle-aircraft_indinana-USA_ifly_flight-school_pilot-training_ifly_logo_aviation_group_blanco_ok.webp",
+    alt: "iFly Logo Aviation Group White",
+  },
+];
